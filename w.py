748a07ab93d700497fe7c6776d875d914cb3d940
luazars <werbungmaef@gmail.com>
2023-03-26 18:46:23 +0200

    new W function

--- w.py
+++ w.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 
 
 N = 500
-h = .1
+h = 100
 m = 1
 
 pos = np.random.randn(N,3)*100
 p = np.zeros(N)
 
 def W(r, h):
-    return (1 / (h * np.sqrt(2*np.pi))) * np.exp((-(r**2)/2*h**2))
+    return (1 / ((h**3) * (np.pi**(2/3)))) * np.exp((-(np.abs(r)**2)/h**2))
 
 r_values = np.linspace(0, 100, 100)
 W_values = W(r_values,h)
