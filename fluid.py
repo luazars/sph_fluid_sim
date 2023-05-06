9ca0876887646bab98b2ebea81ac430de2dc5565
luazars <werbungmaef@gmail.com>
2023-05-06 16:52:48 +0200

    real values with unit

--- fluid.py
+++ fluid.py
@@ -3,43 +3,25 @@ import pygame
 from math import pi
 import random
 
-#size of the screen
-size = 1000
-
-#distance between Particles at start
-distance = 15
+#width and height of the screen
+size = 1000 #mm
 
 #size of particle cube
-xSize = 50
-ySize = 10
+xSize = 50 #mm
+ySize = 50 #mm
 
-#pos of particle cube
-xPos = 1/2 * (size-xSize*distance) #center the cube on x-axis 
-yPos = 100
+#distance between Particles at start
+distance = 2 #mm
 
 #number of particles
 N = xSize*ySize
 
-#mass of particles
-mass = 1
-
-#kernel radius, include all surrounding particles of one particle in a grid
-h = 2**(1/2)*distance
-
-#?
-B = 5.0
-#Adiabatenexponent
-m = 2.5
-
-startingPressure = 0.0
-startingDensity = 1.0
-
 #density of particles
-density = np.zeros(N)
+density = np.zeros(N) #u/mm**2
 #pressure of particles
-pressure = np.zeros(N)
+pressure = np.zeros(N) #MPa
 #positions of particles
-pos = np.zeros((N, 2))
+pos = np.zeros((N, 2)) #mm
 
 #setting positions of particles to a random position on the screen
 def setRandomPosition():
@@ -49,6 +31,10 @@ def setRandomPosition():
 
 #set position of particles in a cube
 def setPositionInGrid():
+    #top-left position of particle cube
+    xPos = 1/2 * (size-xSize*distance) #center the cube on x-axis 
+    yPos = 100
+
     for i in range(N):
         x = i%xSize
         y =  int(i/N*ySize)
@@ -57,20 +43,36 @@ def setPositionInGrid():
 
 #kernel function / W function
 def W(r):
+    #kernel radius, include all surrounding particles of one particle in a grid
+    h = 2**(1/2)*distance #mm
+
     return (1 / ((h**3) * (np.pi**(2/3)))) * np.exp((-(np.abs(r)**2)/h**2))
 
 #calculate density of all particles
 def calculateDensity():
+
+    #mass of water particles
+    mass =  18.01528 #u
+
     for i in range(N):
         for j in range(N):
-            if(i!=j):
-                r = np.linalg.norm(pos[i]-pos[j])
-                density[i] += mass * W(r)
+            r = np.linalg.norm(pos[i]-pos[j])
+            density[i] += mass * W(r)
+        
 
 #calculate pressure with density of all particles with tait equation
 def calculatePressure():
+    #?
+    B = 321.4 #MPa
+    #Adiabatenexponent
+    m = 7 
+
+    pressureReference = 2060.0 #MPa
+    densityReference = 6.004  #komische einheit
+    
     for i in range (N):
-        pressure[i] = startingPressure + B * ((density[i] / startingDensity) ** m - 1)   
+        pressure[i] = pressureReference + B * ((density[i] / densityReference) ** m - 1) #MPa
+        print(pressure[i])
 
 def showWindow():
     #initialization of pygame 
@@ -93,7 +95,7 @@ def showWindow():
 
         #drawing each particle with color according to the density and radius according to pressure
         for i in range(N):
-            pygame.draw.circle(screen, [ (density[i]-min(density)) /(max(density)-min(density))*255,0,0 ], [pos[i][0], pos[i][1]], ((pressure[i]-min(pressure)) /(max(pressure)-min(pressure)))*5+4)
+            pygame.draw.circle(screen, [(density[i]-min(density)) /(max(density)-min(density))*255,0,0 ], [pos[i][0], pos[i][1]], ((pressure[i]-min(pressure)) /(max(pressure)-min(pressure)))*5+4)
 
         #update screen
         pygame.display.flip()
